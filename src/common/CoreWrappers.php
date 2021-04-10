23bd34411b3ccac21b4cad95e2ab1d6b926e70be
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2021-04-10 19:47:49 +0200

    Empathy Engine 0.1.2 - Исправлено выполнение методов класса NetObject

--- src/common/CoreWrappers.php
+++ src/common/CoreWrappers.php
@@ -104,8 +104,9 @@ class NetObject implements \ArrayAccess
      */
     public function __call (string $name, array $args)
     {
-        return Additions::coupleSelector ($this->callMethod ($name,
-            array_map ('Empathy\Engine\Additions::uncoupleSelector', $args)));
+        return method_exists ($this, $name) ?
+            call_user_method ($name, $this, ...$args) :
+            Additions::coupleSelector ($this->callMethod ($name, array_map ('Empathy\Engine\Additions::uncoupleSelector', $args)));
     }
 
     /**
